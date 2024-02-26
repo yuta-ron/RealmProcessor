d9a19bbd09964edb15f7a0162e812e288300714c
yuta-ron <jan.joqr@gmail.com>
2024-02-27 00:51:01 +0900

    fix

--- RealmProcessor/main.swift
+++ RealmProcessor/main.swift
@@ -12,5 +12,24 @@ print("Hello, World!")
 
 
 let loc = Location()
-
-print(loc)
+// Realmインスタンスの取得
+let realm = try! Realm()
+do {
+    let fm = FileManager.default
+    //let curdir = fm.currentDirectoryPath
+    let curDir = "/Users/yutaron/ghq/github.com/yuta-ron/RealmProcessor"
+    let prefsDir = URL(fileURLWithPath: (curDir as NSString).appendingPathComponent("prefs"))
+    // 指定したディレクトリ内のアイテムのURLを取得
+    let fileURLs = try fm.contentsOfDirectory(at: prefsDir, includingPropertiesForKeys: nil)
+    
+    print(fileURLs)
+    // .csv拡張子を持つファイルのみをフィルタリング
+    let dirs = fileURLs.filter { $0.pathExtension != "zip" }
+    // フィルタリングされたCSVファイルのパスを表示
+    for dirname in dirs {
+        let fileURLs = try fm.contentsOfDirectory(at: dirname, includingPropertiesForKeys: nil)
+//        print(fileURL.path)
+    }
+} catch  {
+    print("error: \(error)")
+}
